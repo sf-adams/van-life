19f9407df4c132ddfd0b6f26fbee957c99999b44
Sam <samuel.fitz@btinternet.com>
2023-11-24 18:40:13 +0000

    feat: add homepage styling and restructure root layout

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -2,13 +2,13 @@ import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
-    <div>
-      <h1>You got the travel plans, we got the travel vans.</h1>
-      <p>
+    <main className="home">
+      <h1 className="home__title">You got the travel plans, we got the travel vans.</h1>
+      <p className="home__description">
         Add adventure to your life by joining the #vanlife movement. Rent the
         perfect van to make your perfect road trip.
       </p>
-      <Link to="/vans">Find your van</Link>
-    </div>
+      <Link to="/vans" className="home__link">Find your van</Link>
+    </main>
   );
 }
--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,17 +1,14 @@
 import { Outlet } from "react-router-dom";
 
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 export default function Root() {
   return (
     <>
       <Header />
-      <main>
-        <Outlet />
-      </main>
-      <footer>
-        <p>© 2023 #VANLIFE</p>
-      </footer>
+      <Outlet />
+      <Footer />
     </>
   );
 }
