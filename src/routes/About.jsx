1061644339d7efc330a08125e5c6e8a268fce530
Sam <samuel.fitz@btinternet.com>
2023-11-24 19:32:27 +0000

    feat: add about page styling and content

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,28 +1,35 @@
 import { Link } from "react-router-dom";
+import AboutHero from "../assets/about-image.png";
 
 export default function About() {
   return (
-    <div>
+    <main className="about">
       <img
-        src="../assets/about-image.png"
+        className="about__hero"
+        src={AboutHero}
         alt="Person sat on camper van looking up at the night sky"
       />
-      <h2>Don&apos;t squeeze in a sedan when you could relax in a van.</h2>
-      <p>
-        Our mission is to enliven your road trip with the perfe t travel van
-        rental. Our vans are recertified befgore each trip to ensure your travel
-        plans can go off without a hitch. (Hitch costs extra)
-      </p>
-      <p>
-        Our team is full of vanlife enthusiasts who know firsthand the magic of
-        touring the world on 4 wheels.
-      </p>
-
-      <div>
-        <h4>You destination is waiting.</h4>
-        <h4>Your van is ready.</h4>
-        <Link to="/vans">Explore our vans</Link>
-      </div>
-    </div>
+      <section className="about__container">
+        <h2 className="about__title">
+          Don&apos;t squeeze in a sedan when you could relax in a van.
+        </h2>
+        <p className="about__description">
+          Our mission is to enliven your road trip with the perfect travel van
+          rental. Our vans are recertified before each trip to ensure your travel
+          plans can go off without a hitch. (Hitch costs extra 😉)
+        </p>
+        <p className="about__description">
+          Our team is full of vanlife enthusiasts who know firsthand the magic of
+          touring the world on 4 wheels.
+        </p>
+        <div className="about__callout">
+          <h4 className="about__callout-text">Your destination is waiting.</h4>
+          <h4 className="about__callout-text">Your van is ready.</h4>
+          <Link to="/vans" className="button about__link">
+            Explore our vans
+          </Link>
+        </div>
+      </section>
+    </main>
   );
 }
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -8,7 +8,7 @@ export default function Home() {
         Add adventure to your life by joining the #vanlife movement. Rent the
         perfect van to make your perfect road trip.
       </p>
-      <Link to="/vans" className="home__link">Find your van</Link>
+      <Link to="/vans" className="button home__link">Find your van</Link>
     </main>
   );
 }
